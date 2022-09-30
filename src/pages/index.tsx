f2bc280642e39fec50a65e5bfd99a5676193c7ba
Jaka Stavanja <jakastavanja@gmail.com>
2022-10-01 00:00:35 +0200

    add github auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
+import { useSession, signOut } from "next-auth/react";
+import Link from "next/link";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { data: session } = useSession();
+  return (
+    <div>
+      <h1>Welcome to traken</h1>
+      {session?.user && (
+        <>
+          <p>Logged in as {session?.user?.email}</p>
+          <button
+            onClick={() => {
+              signOut();
+            }}
+          >
+            Log out
+          </button>
+        </>
+      )}
+      {!session?.user && (
+        <Link href="/api/auth/signin">
+          <button>Sign in</button>
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Home;
