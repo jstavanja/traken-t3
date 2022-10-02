1c00a6cbf04ff945a85d4f8a309fdbb59fa881c1
Jaka Stavanja <jakastavanja@gmail.com>
2022-10-02 21:52:06 +0200

    prettify the explore song page to some extent

--- src/pages/explore/[id].tsx
+++ src/pages/explore/[id].tsx
@@ -2,6 +2,87 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import AuthGuard from "../../components/AuthGuard";
 import { trpc } from "../../utils/trpc";
+import { Table, Button, Container } from "@mantine/core";
+
+import { createStyles, Group, Paper, SimpleGrid, Text } from "@mantine/core";
+import {
+  IconUserPlus,
+  IconDiscount2,
+  IconReceipt2,
+  IconCoin,
+  IconArrowUpRight,
+  IconArrowDownRight,
+  IconMusic,
+} from "@tabler/icons";
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    padding: theme.spacing.xl * 1.5,
+  },
+
+  value: {
+    fontSize: 24,
+    fontWeight: 700,
+    lineHeight: 1,
+  },
+
+  diff: {
+    lineHeight: 1,
+    display: "flex",
+    alignItems: "center",
+  },
+
+  icon: {
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark?.[3]
+        : theme.colors.gray?.[4],
+  },
+
+  title: {
+    fontWeight: 700,
+    textTransform: "uppercase",
+  },
+}));
+
+const icons = {
+  music: IconMusic,
+};
+
+interface TracksGridProps {
+  data: { title: string; icon: keyof typeof icons }[];
+}
+
+export function TracksGrid({ data }: TracksGridProps) {
+  const { classes } = useStyles();
+  const stats = data.map((tracks) => {
+    const Icon = icons[tracks.icon];
+
+    return (
+      <Paper withBorder p="md" radius="md" key={tracks.title}>
+        <Group position="apart">
+          <Text size="xs" color="dimmed" className={classes.title}>
+            {tracks.title}
+          </Text>
+          <Icon className={classes.icon} size={22} stroke={1.5} />
+        </Group>
+      </Paper>
+    );
+  });
+  return (
+    <div className={classes.root}>
+      <SimpleGrid
+        cols={4}
+        breakpoints={[
+          { maxWidth: "md", cols: 2 },
+          { maxWidth: "xs", cols: 1 },
+        ]}
+      >
+        {stats}
+      </SimpleGrid>
+    </div>
+  );
+}
 
 const ViewCompositionPage = () => {
   const router = useRouter();
@@ -36,29 +117,30 @@ const ViewCompositionPage = () => {
 
   return (
     <AuthGuard>
-      <div>
+      <Container>
         {composition && (
           <>
-            <h1>Viewing composition: {composition.name}</h1>
+            <Group>
+              <h1>Viewing composition: {composition.name}</h1>
+              {composition.currentUserHasAccess && (
+                <Link href={`/play/${composition.id}`}>
+                  <Button>Play this composition</Button>
+                </Link>
+              )}
+              {!composition.currentUserHasAccess && (
+                <button onClick={buyComposition}>Buy this composition</button>
+              )}
+            </Group>
             <h2>All tracks in composition</h2>
-            {composition.currentUserHasAccess && (
-              <Link href={`/play/${composition.id}`}>
-                <button>Play this composition</button>
-              </Link>
-            )}
-            {!composition.currentUserHasAccess && (
-              <button onClick={buyComposition}>Buy this composition</button>
-            )}
-            <div>
-              {composition.tracks?.map((track, idx) => (
-                <div key={track.name + idx}>
-                  <h4>{track.name}</h4>
-                </div>
-              ))}
-            </div>
+            <TracksGrid
+              data={composition.tracks.map((track) => ({
+                title: track.name,
+                icon: "music",
+              }))}
+            />
           </>
         )}
-      </div>
+      </Container>
     </AuthGuard>
   );
 };
